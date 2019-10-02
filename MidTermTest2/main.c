8f90f9d6f10482339734e914e8710089b45ac4b4
KaitlynHuynh <54717858+KaitlynHuynh@users.noreply.github.com>
2019-10-02 08:38:05 -0700

    Initial Commit

---
+++ MidTermTest2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MidTermTest2
+//
+//  Created by Huynh, Kaitlyn on 10/2/19.
+//  Copyright © 2019 Huynh, Kaitlyn. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
