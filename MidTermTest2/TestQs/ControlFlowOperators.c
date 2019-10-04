caf6678178342ed170bb39c7948e0b5919f9d039
gwen <gavestal@gmail.com>
2019-10-04 08:16:35 -0700

    commit

---
+++ MidTermTest2/TestQs/ControlFlowOperators.c
@@ -0,0 +1,9 @@
+//
+//  ControlFlowOperators.c
+//  MidTermTest2
+//
+//  Created by Vestal, Gwendolyn on 10/4/19.
+//  Copyright © 2019 Huynh, Kaitlyn. All rights reserved.
+//
+
+#include "testqs.h"
