568159833e937aebdccf42572dd9339a45244975
Alex Titov <alextitov203@gmail.com>
2019-10-04 09:10:55 -0700

    finished q

--- MidTermTest2/TestQs/ControlFlowOperators.c
+++ MidTermTest2/TestQs/ControlFlowOperators.c
@@ -8,13 +8,29 @@
 
 #include "testqs.h"
 
-int controlflowoperators(){
-
-    printf ("   int num = 5;");
-    printf ("for (int x = 0; x < 10; x++) {");
-    printf (")    num +=x;");
-    printf ("}");
-    printf ("return num;");
+void controlflowoperators(){
+    printf("int control(int num)\n");
+    printf ("for (int x = 0; x < 10; x++\n) {");
+    printf (")    num +=x;\n");
+    printf ("}\n");
+    printf ("return num;\n");
     
-    return 0;
+    printf("What would control(7) return?\n");
+    printf("a: 52\nb:33\nc:51\nd:53");
+    char ans;
+    scanf("%c", &ans);
+    switch (ans) {
+        case 'a':
+        case 'A':
+        printf("Correct!\n");
+        case 'b':
+        case 'B':
+        printf("Incorrect: \n");
+        case 'c':
+        case 'C':
+        printf("Incorrect: \n");
+        case 'd':
+        case 'D':
+        printf("Incorrect: \n");
+    }
 }
