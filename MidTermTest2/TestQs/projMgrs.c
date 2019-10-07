6460af872533e6e5c523c964e41df57c4e747f9e
Navodit Maheshwari <navoditmaheshwari@gmail.com>
2019-10-07 08:48:40 -0700

    ProjMngs additions

--- MidTermTest2/TestQs/projMgrs.c
+++ MidTermTest2/TestQs/projMgrs.c
@@ -9,24 +9,35 @@
 #include <stdio.h>
 int projMgrs(void)
 {
-    char ans;
+    
+    int count=0;
     printf("Which number would call the first character in a array\n");
     printf("A. 0\nB. 1\nC. 2\nD. Array1\n");
-    
+    for(int loop=0;loop<1;){
+        char ans;
     scanf("%c", &ans);
     switch (ans) {
         case 'a':
         case 'A':
             printf("Correct!\n");
+            loop++;
         case 'b':
         case 'B':
             printf("Incorrect: \n");
+             loop++;
         case 'c':
         case 'C':
             printf("Incorrect: \n");
+             loop++;
         case 'd':
         case 'D':
             printf("Incorrect: \n");
+             loop++;
+            default:
+            puts("This answer is invalid. Try Again.\n");
+            count++;
+            printf("You have put something random %d times", count);
+    }
     }
     return 0;
 }
